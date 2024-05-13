3458f2fa74d3c14715fc9ce38fbc84775e015f60
helder mabasso <hfmabasso@gmail.com>
2024-05-14 00:22:54 +0200

    Starter Files

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  //todos state with initial value as an array of objects
+  const [todos, setTodo] = useState([
+
+
+    {
+      text: 'learn react',
+      isCompleted: false,
+    }
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build react',
+      isCompleted: false,
+    }        
+  ])
+
+
+  //input value state
+  const [value,setValue] = React.useState('');
+
+ 
+  //removes a task from the list
+  
+
+
+
+ 
+  return (
+    <>
+        {
+          todos.map((todo,i) => <div className="todo" key={i} id={i} >{todo.text}</div>)
+        }
+
+        <form >
+
+          <input type="text" value={value} placeholder="Add new todo" />
+
+        </form>
+
+        
+    </>
+  )
+}
+
+export default App
